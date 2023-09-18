7ef1391b91148de61f174badeebd5c53b8f0b3a1
cafeasp <vpacheco99@gmail.com>
2023-09-18 17:59:40 -0400

    Write data to excel sheet

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,47 @@
 var router = require("express").Router();
-
+const axios = require("axios");
+const dotenv = require("dotenv");
+dotenv.config();
 router.get("/", function (req, res, next) {
   res.render("index");
 });
+
+router.post("/weather", function (req, res, next){
+  const params = {
+    key: `${process.env.WEATHER_KEY}`,
+    q: `${req.body.zip}`,days:1
+  };
+let url = process.env.URL;
+
+  try {
+    axios.get(url,{ params })
+    .then(function (response) {
+     
+      let data = {location:response.data.location.name,temperature:response.data.current.temp_f,
+        condition:response.data.current.condition.text,icon:response.data.current.condition.icon,
+        forcast_icon:response.data.forecast.forecastday[0].day.condition.icon,
+        forcast_condition:response.data.forecast.forecastday[0].day.condition.text,
+        forcast_high:response.data.forecast.forecastday[0].day.maxtemp_f,
+        forcast_low:response.data.forecast.forecastday[0].day.mintemp_f,
+        daily_chance_of_rain:response.data.forecast.forecastday[0].day.daily_chance_of_rain};
+
+      res.send(data);
+    })
+    .catch(function (error) {
+      // handle error
+      
+      res.send(error);
+    })
+    .finally(function () {
+      console.log("Weather API call complete");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+
+
 
 module.exports = router;
